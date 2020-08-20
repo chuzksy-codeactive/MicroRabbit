08c1abe66d1877cc3201b6b68ba09ba1b75b2e18
chuzksy-codeactive <chuzksy@gmail.com>
2020-08-20 08:32:56 +0100

    - Create Services in the Application layer - Create DbContext and  Repository in the Data layer - Update the IoC container - Add connection string

---
+++ MicroRabbit.Banking.Data/Repository/AccountRepository.cs
@@ -0,0 +1,23 @@
+﻿using MicroRabbit.Banking.Data.Context;
+using MicroRabbit.Banking.Domain.Interfaces;
+using MicroRabbit.Banking.Domain.Models;
+using System;
+using System.Collections.Generic;
+
+namespace MicroRabbit.Banking.Data.Repository
+{
+    public class AccountRepository : IAccountRepository
+    {
+        private readonly BankingDbContext _ctx;
+
+        public AccountRepository(BankingDbContext ctx)
+        {
+            _ctx = ctx ?? throw new ArgumentNullException(nameof(ctx));
+        }
+
+        public IEnumerable<Account> GetAccounts()
+        {
+            return _ctx.Accounts;
+        }
+    }
+}
--- MicroRabbit.Infra.IoC/DependencyContainer.cs
+++ MicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -1,6 +1,11 @@
 ﻿using MicroRabbit.Domain.Core.Bus;
 using Microsoft.Extensions.DependencyInjection;
 using MicroRabbit.Infra.Bus;
+using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Application.Services;
+using MicroRabbit.Banking.Domain.Interfaces;
+using MicroRabbit.Banking.Data.Repository;
+using MicroRabbit.Banking.Data.Context;
 
 namespace MicroRabbit.Infra.IoC
 {
@@ -8,7 +13,15 @@ namespace MicroRabbit.Infra.IoC
     {
         public static void RegisterServices(IServiceCollection services)
         {
+            // Domain Bus
             services.AddTransient<IEventBus, RabbitMQBus>();
+
+            // Application Services
+            services.AddTransient<IAccountService, AccountService>();
+
+            // Application Data
+            services.AddTransient<IAccountRepository, AccountRepository>();
+            services.AddTransient<BankingDbContext>();
         }
     }
 }
